9669acff8720cc72afd006cf2b8dbe0dc38c7c50
priya8957-dell <maddeshiyapriyanshu2@gmail.com>
2026-04-04 15:36:14 +0530

    render error solve

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import UserContext from './context/UserContext.jsx'
-import CaptainContext from './context/CaptainContext.jsx'
-import SocketProvider from './context/SocketContext.jsx'
+import UserContext from './context/UserContext'
+import CaptainContext from './context/CaptainContext'
+import SocketProvider from './context/SocketContext'
 
 import "leaflet/dist/leaflet.css";
 
